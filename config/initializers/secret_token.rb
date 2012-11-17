92527ca11529e1b46f6f09f447fdc903eeea8614
Scott Burton <scottburton11@gmail.com>
2012-11-16 23:21:48 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fauxfile::Application.config.secret_token = '4e33d06e9b057935f66956f8bb8a89bb77f0844f132c302c05ecdb83477b074199024b2299db2a5270f00d4e1b5a22fb412701543763db48defa3f5a74f59534'
